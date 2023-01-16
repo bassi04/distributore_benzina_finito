cbfc7fb3977b1e2e523db13f0dd946399bfe1dcc
Bassi.18173 <Bassi.18173@LABI1-6PC14>
2023-01-16 10:00:22 +0100

    Aggiungere i file di progetto.

---
+++ distributore_benzina_finito/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using distributore_benzina_finito;
+
+namespace distributorre_di_benzina
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                distributore_benzina distributore = new distributore_benzina("dkjfh2", "via caio", 100);//INFORMAZIONI inserite nel custrottore 
+                int c = 0;
+                Console.WriteLine("scegliere opzione");
+                do
+                {
+                    Console.WriteLine("1-caricare soldi");
+                    Console.WriteLine("2-impostare prezzo al litro");
+                    Console.WriteLine("3-riempire il distrributore");
+                    Console.WriteLine("4-erogare benzina");
+                    Console.WriteLine("5-confrontare due distributori");
+
+                    c = int.Parse(Console.ReadLine());
+                    if (c == 1)
+                    {
+                        int soldi;
+                        Console.WriteLine("inserisci soldi");
+                        soldi = int.Parse(Console.ReadLine());
+                        bool controllo = true;
+                        while (controllo == true)
+                        {
+                            int a;
+                            Console.WriteLine("vuoi inserire altri soldi ? 1=si 2=no");
+                            a = int.Parse(Console.ReadLine());
+                            if (a == 1)
+                            {
+                                int b = int.Parse(Console.ReadLine());
+                                soldi = soldi + b;
+                                b = 0;
+                            }
+                            if (a == 2)
+                            {
+                                distributore.AggiungiDenaro(soldi);//passo i dati nella funzione nella classe
+                                controllo = false;
+                            }
+                            Console.WriteLine(distributore.Denaro_caricato);// per avviare il programma schiaccia ctrl+f5 se no termina subito 
+                        }
+
+                    }
+                    if (c == 2)
+                    {
+                        Console.WriteLine("immettere il prezzo al litro");
+                        distributore.Prezzo_litro = int.Parse(Console.ReadLine());
+                        Console.WriteLine("il prezzo al litro è di " + distributore.Prezzo_litro);
+
+                    }
+                    if (c == 3)
+                    {
+                        Console.WriteLine("di quanto vuoi riempire il distributore ?");
+                        int litri = 0;
+                        litri = int.Parse(Console.ReadLine());
+                        distributore.ControlloRiempireDistributore(litri);
+
+
+                    }
+                    if (c == 4)
+                    {
+                        Console.WriteLine("quanti litri di benzina vuoi prelevare?");
+                        int benzina_erogata = int.Parse(Console.ReadLine());
+                        //int benzina_erogata = distributore.Denaro_caricato / distributore.Prezzo_litro;
+                        distributore.ControlloLivelloBenzinaErogazione(benzina_erogata);
+
+
+
+                    }
+
+                    if (c == 5)
+                    {
+                        distributore_benzina distributore2 = new distributore_benzina("dkjfh2", "via caio", 100);
+                        Console.WriteLine("inserisci costo benzina al litro del distributore2");
+                        distributore2.Prezzo_litro = int.Parse(Console.ReadLine());
+                        distributore2 = distributore2.confrontaDistributorMinore(distributore);
+                        Console.WriteLine(distributore2.Prezzo_litro);
+
+
+
+                    }
+                } while (c >= 1 && c <= 5);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
---
+++ distributore_benzina_finito/distributore_benzina.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace distributore_benzina_finito
+{
+    public class distributore_benzina
+    {
+        private string _numero_seriale;
+        private string _indirizzo;
+        private int _capacita_massima; // la quantita massima di benzina che può contenere il distributore 
+        private int _livello_benzina; // la quantita di benzina presente in questo momento 
+        private int _denaro_caricato;
+        private int _prezzo_litro;
+
+        public string Numero_seriale { get { return _numero_seriale; } private set { _numero_seriale = value; } }
+        public string Indirizzo { get { return _indirizzo; } set { _indirizzo = value; } }
+        public int Capacita_massima { get { return _capacita_massima; } private set { _capacita_massima = value; } }
+        public int Livello_benzina { get { return _livello_benzina; } set { _livello_benzina = value; } }
+        public int Denaro_caricato { get { return _denaro_caricato; } set { _denaro_caricato = value; } }
+        public int Prezzo_litro
+        {
+            get
+            {
+
+                return _prezzo_litro;
+
+            }
+            set
+            {
+                _prezzo_litro = value;
+            }
+        }
+        public distributore_benzina(string _numero_seriale, string _indirizzo, int _capacita_massima) // costruttore dove vanno dichiarate le variabili che non possono essere cambiate dal utente
+        {
+            this.Numero_seriale = _numero_seriale;
+            this.Indirizzo = _indirizzo;
+            this.Capacita_massima = _capacita_massima;
+
+
+        }
+
+        public distributore_benzina(string _numero_seriale) : this(_numero_seriale, "", 100)
+        {
+
+        }
+
+        public void AggiungiDenaro(int soldi)
+        {
+            if (soldi < 0)
+            {
+                throw new Exception("numero negativo");
+            }
+
+
+            Denaro_caricato = soldi;
+
+
+            ControlloDenaro(Denaro_caricato);
+
+
+
+        }
+
+        public void ControlloDenaro(int Denaro_caricato)
+        {
+            if (Denaro_caricato >= 5 && Denaro_caricato <= 100)
+            {
+
+            }
+            else
+            {
+
+                throw new Exception("inserire credito maggiore di 5 ma minore di 100"); //non fa proseguire l'utente 
+            }
+
+
+
+        }
+
+        public void ControlloRiempireDistributore(int litri)
+        {
+            if (litri < 0)
+            {
+                throw new Exception("numero negativo");
+            }
+            LvelloBenzinaIniziale();
+            Livello_benzina = Livello_benzina + litri;
+
+            if (Livello_benzina > Capacita_massima)
+            {
+                throw new Exception("il distributore si riempirebbe troppo");
+            }
+
+
+        }
+
+        public void ControlloLivelloBenzinaErogazione(int benzina_erogata)
+        {
+
+            if (benzina_erogata < 0)
+            {
+                throw new Exception("numero negativo");
+            }
+
+            LvelloBenzinaIniziale();
+
+            if (benzina_erogata / Prezzo_litro > Denaro_caricato / Prezzo_litro)
+            {
+                throw new Exception("soldi non sufficenti ");
+
+            }
+
+
+            Livello_benzina = Livello_benzina - benzina_erogata / Prezzo_litro;
+
+
+            if (Livello_benzina < 0)
+            {
+                throw new Exception("siamo spiacenti ma il distributore non ha abbastana benzina ");
+
+            }
+
+            if (benzina_erogata == 0)
+            {
+                throw new Exception("verificare se si ha inserito il denaro e se si ha impostato il prezzo a litro ");
+            }
+
+
+            Console.WriteLine(Livello_benzina);
+
+        }
+
+
+        public distributore_benzina confrontaDistributorMinore(distributore_benzina b)
+        {
+            if (this.Prezzo_litro < b.Prezzo_litro)
+            {
+                return this;
+            }
+            return b;
+
+        }
+
+        public void LvelloBenzinaIniziale()
+        {
+            int a = 0;
+            if (Livello_benzina == 0 && a == 0)
+            {
+                Livello_benzina = Capacita_massima;
+                a = 1;
+            }
+            else
+            {
+
+            }
+        }
+        /* public int ToString()
+         {
+             return Prezzo_litro;
+         }*/
+    }
+}
